eaa1a08b0b1b82d065a6464486d0b4473127897b
Pavlo Aksonov <aksonov@gmail.com>
2016-06-24 14:02:00 +0200

    fix dependencies

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.SocketSCXMLListener = exports.Transition = exports.State = exports.StateMachine = undefined;
+exports.Transition = exports.State = exports.StateMachine = undefined;
 
 var _StateMachine = require('./StateMachine');
 
@@ -17,13 +17,8 @@ var _Transition = require('./Transition');
 
 var _Transition2 = _interopRequireDefault(_Transition);
 
-var _SocketSCXMLListener = require('./SocketSCXMLListener');
-
-var _SocketSCXMLListener2 = _interopRequireDefault(_SocketSCXMLListener);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.StateMachine = _StateMachine2.default;
 exports.State = _State2.default;
 exports.Transition = _Transition2.default;
-exports.SocketSCXMLListener = _SocketSCXMLListener2.default;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import StateMachine from './StateMachine';
 import State from './State';
 import Transition from './Transition';
-import SocketSCXMLListener from './SocketSCXMLListener';
 
 export {
   StateMachine,
   State,
   Transition,
-  SocketSCXMLListener,
 }
